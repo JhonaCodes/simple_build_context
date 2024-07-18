b12ae45d0add7264c80584f161cf4e2b4d162263
Jhonacode <jhonacode2020@gmail.com>
2024-07-18 22:56:04 +1200

    Update SDK version

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jhonatanortiz/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Data/Flutter/Librarys/simple_build_context"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.22.0/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Data/Private/04_Librarys/simple_build_context"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.6"
+export "FLUTTER_BUILD_NUMBER=1.0.6"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
